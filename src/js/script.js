64e090e2f567d9b36ec626e2066cb8aa4e6962ab
RhyanVictor <rhyansinho.04@gmail.com>
2024-09-03 18:59:09 -0400

    correção java script

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,8 @@ const minutos = document.getElementById("minutos");
 const segundos = document.getElementById("segundos");
 
 const relogio = setInterval(function time() {
-let dateToday = new Date();
-let hr = dateToday.getHours();
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
     let s = dateToday.getSeconds();
 
